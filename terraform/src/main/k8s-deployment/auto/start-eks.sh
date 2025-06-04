e0aa154ffdef5de3a5754f4ef4e509c654594378
Yuhwan Kim <yhkim8046@gmail.com>
2025-06-05 01:45:46 +0900

    terraform:   - Automate scale EKS setup

---
+++ terraform/src/main/k8s-deployment/auto/start-eks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CLUSTER_NAME="Eouil-eks-cluster"
+NODEGROUP_NAME="app-20250604141555311800000001"
+REGION="ap-northeast-2"
+
+echo "Starting EKS NodeGroup..."
+aws eks update-nodegroup-config \
+  --cluster-name $CLUSTER_NAME \
+  --nodegroup-name $NODEGROUP_NAME \
+  --scaling-config minSize=1,maxSize=3,desiredSize=1 \
+  --region $REGION
+
+echo "NodeGroup scale-up complete."
